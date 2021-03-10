b0f74e293717106036b2e6cae7982b6d86faff96
Samuel Otero <samuel.otero@live.com>
2021-03-10 17:09:16 -0500

    Added LayoutBuilder for LineItemEntry Widget so we can have different looks depending on screen width

--- lib/widgets/TimeLineEntry.dart
+++ lib/widgets/TimeLineEntry.dart
@@ -1,4 +1,5 @@
 import 'package:check_to_check/core/models/TimeLineEntryModel.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class TimeLineEntry extends StatelessWidget {
@@ -10,11 +11,24 @@ class TimeLineEntry extends StatelessWidget {
   void _handleToggle(bool value) {
       onToggled(model.id);
   }
+
   @override
   Widget build(BuildContext context) {
     final amount = model.isIncome ? "+ "+model.amount : model.amount;
     final subTitleStyle = new TextStyle(fontWeight: FontWeight.bold);
     final TextStyle amountStyle = model.isIncome ? new TextStyle(fontStyle: FontStyle.italic, color: Color(0xff07ce07), fontWeight: FontWeight.bold) : subTitleStyle;
+    return LayoutBuilder(
+        builder: (context, constraints) {
+      if (constraints.maxWidth > 600) {
+        return _buildWide(amount, subTitleStyle, amountStyle);
+      } else {
+        return _buildCompact(amount, subTitleStyle, amountStyle);
+      }
+    });
+  }
+
+  //Used for wide displays
+  Widget _buildWide(String amount, TextStyle subTitleStyle, TextStyle amountStyle) {
     return CheckboxListTile(
       value: model.enabled,
       controlAffinity: ListTileControlAffinity.leading,
@@ -27,10 +41,10 @@ class TimeLineEntry extends StatelessWidget {
           mainAxisSize: MainAxisSize.max,
           children: [
             Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-              children: [Text(model.date, style: subTitleStyle)],
-            )),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [Text(model.date, style: subTitleStyle)],
+                )),
             Expanded( child:Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [Text(amount, style: amountStyle)],
@@ -42,8 +56,44 @@ class TimeLineEntry extends StatelessWidget {
           ]),
       secondary: IconButton(
         icon: Icon(
-        Icons.edit_outlined,
-        color: Colors.blueAccent),
+            Icons.edit_outlined,
+            color: Colors.blueAccent),
+        onPressed: (){
+          debugPrint("Clicked on edit");
+        },
+      ),
+      onChanged: (value) {
+        _handleToggle(value);
+      },
+    );
+  }
+
+  //Used for smaller displays
+  Widget _buildCompact(String amount, TextStyle subTitleStyle, TextStyle amountStyle) {
+    return CheckboxListTile(
+      value: model.enabled,
+      controlAffinity: ListTileControlAffinity.leading,
+      title: Text(
+        model.description + " "+model.date,
+        style: _biggerFont,
+      ),
+      subtitle: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Expanded( child:Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [Text(amount, style: amountStyle)],
+            )),
+            Expanded( child:Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [Text(model.newTotal, style: subTitleStyle)],
+            )),
+          ]),
+      secondary: IconButton(
+        icon: Icon(
+            Icons.edit_outlined,
+            color: Colors.blueAccent),
         onPressed: (){
           debugPrint("Clicked on edit");
         },
